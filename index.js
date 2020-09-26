f72a7f040da543bdae7ea4820857835662c5387a
Nitioilar210 <71893343+Nitioilar210@users.noreply.github.com>
2020-09-26 20:53:00 +0700

    index

---
+++ index.js
@@ -0,0 +1,25 @@
+// /**
+//  * @format
+//  */
+
+// import {AppRegistry, Button} from 'react-native';
+// import App from './App';
+// import {name as appName} from './app.json';
+// import LotsOfStyles from './src/LotOfStyles';
+// import FixedDimension from './src/FixedDimension';
+// import FlexDimension from './src/FlexDimension';
+// import FlexDirection from './src/FlexDirection';
+// import MyImage from './src/MyImage';
+// import ButtonBasic from './src/ButtonBasic';
+// import ListDemo from './src/ListDemo';
+// import ModalDemo from './src/ModalDemo';
+// import TouchableButton from './src/TouchableButton';
+// import IconDemo from './src/IconDemo';
+// import PickerDemo from './src/PickerDemo';
+
+
+// AppRegistry.registerComponent(appName, () => MyImage);
+// import ScreenManager from './src/screens/ScreenManager';
+// import PjScreenManager from './src/screens/Pj/PjSceenManager';
+import ExamManager from './src/screens/Exam/ExamManager'
+// Navigation.events().registerAppLaunchedListener(() =>{ScreenManager});
